aa29a29194b69061b7670665938c12fa30ff8ea9
Anuja Mutalik <a.mutalik@zera.de>
2022-07-15 17:47:39 +0200

    Prettify cmdhandlerzfpgatest
    Signed-off-by: Anuja Mutalik <a.mutalik@zera.de>

--- src/cmdhandlerzfpgatest.cpp
+++ src/cmdhandlerzfpgatest.cpp
@@ -27,38 +27,30 @@ void CmdHandlerZfpgaTest::StartCmd(SimpleCmdData *pCmd, QVariantList params)
     case CMD_ZFPGATEST_READ:
     case CMD_ZFPGATEST_READ_ASCII:
         ui32Address = hexStringToInt(params[0]);
-        if (lSeek(ui32Address, gDeviceFd))
-        {
+        if (lSeek(ui32Address, gDeviceFd)) {
             emit reportlSeekError();
             return;
         }
         ui32Len = params[1].toInt();
         readData.resize(ui32Len);
-        if(read(gDeviceFd, readData.data(), ui32Len) < 0)
-        {
+        if(read(gDeviceFd, readData.data(), ui32Len) < 0) {
             emit OperationFinish(true, QLatin1String("read did not succeed"));
             return;
         }
-        if(pCmd->GetCmdID() == CMD_ZFPGATEST_READ)
-        {
-            for(int iByte=0; iByte<readData.size(); iByte++)
-            {
+        if(pCmd->GetCmdID() == CMD_ZFPGATEST_READ) {
+            for(int iByte=0; iByte<readData.size(); iByte++) {
                 strByteData.sprintf("%02X", (quint8)readData[iByte]);
                 strResult += strByteData;
                 if(iByte%4 == 3)
                     strResult += QLatin1String(" ");
             }
         }
-        else
-        {
-            for(int iByte=0; iByte<readData.size(); iByte++)
-            {
-                if((quint8)readData[iByte] >= 32 && (quint8)readData[iByte] < 127)
-                {
+        else {
+            for(int iByte=0; iByte<readData.size(); iByte++) {
+                if((quint8)readData[iByte] >= 32 && (quint8)readData[iByte] < 127) {
                     strByteData.sprintf("%c", (int)readData[iByte]);
                 }
-                else
-                {
+                else {
                     strByteData.sprintf("<0x%02X>", (quint8)readData[iByte]);
                 }
                 strResult += strByteData;
@@ -69,23 +61,19 @@ void CmdHandlerZfpgaTest::StartCmd(SimpleCmdData *pCmd, QVariantList params)
     case CMD_ZFPGATEST_WRITE:
     case CMD_ZFPGATEST_WRITE_ASCII:
         ui32Address = hexStringToInt(params[0]);
-        if (lSeek(ui32Address, gDeviceFd))
-        {
+        if (lSeek(ui32Address, gDeviceFd)) {
             emit reportlSeekError();
             return;
         }
-        if(pCmd->GetCmdID() == CMD_ZFPGATEST_WRITE)
-        {
+        if(pCmd->GetCmdID() == CMD_ZFPGATEST_WRITE) {
             QString strData;
             strData = params[1].toString().replace(QLatin1String(" "), QString());
             ui32Len = strData.size() / 2;
-            for(quint32 ui32Byte=0; ui32Byte<ui32Len; ui32Byte++)
-            {
+            for(quint32 ui32Byte=0; ui32Byte<ui32Len; ui32Byte++) {
                 writeData.append(strData.mid(ui32Byte*2, 2).toInt(Q_NULLPTR, 16));
             }
         }
-        else
-        {
+        else {
             writeData = QSimpleCmdParserBase::BinaryFromAscii(params[1].toString());
             ui32Len = writeData.size();
         }
@@ -96,8 +84,7 @@ void CmdHandlerZfpgaTest::StartCmd(SimpleCmdData *pCmd, QVariantList params)
         break;
     case CMD_ZFPGATEST_READ_WITH_TIMER:
         ui32Address = hexStringToInt(params[0]);
-        if (lSeek(ui32Address, gDeviceFd))
-        {
+        if (lSeek(ui32Address, gDeviceFd)) {
             emit reportlSeekError();
             return;
         }
@@ -105,15 +92,13 @@ void CmdHandlerZfpgaTest::StartCmd(SimpleCmdData *pCmd, QVariantList params)
         readData.resize(ui32Len);
 
         readTime.start();
-        if(read(gDeviceFd, readData.data(), ui32Len) < 0)
-        {
+        if(read(gDeviceFd, readData.data(), ui32Len) < 0) {
             emit OperationFinish(true, QLatin1String("read did not succeed"));
             return;
         }
         readTime.stop();
 
-        for(int iByte=0; iByte<readData.size(); iByte++)
-        {
+        for(int iByte=0; iByte<readData.size(); iByte++) {
             strByteData.sprintf("%02X", (quint8)readData[iByte]);
             strResult += strByteData;
             if(iByte%4 == 3)
@@ -127,8 +112,7 @@ void CmdHandlerZfpgaTest::StartCmd(SimpleCmdData *pCmd, QVariantList params)
         break;
     case CMD_ZFPGATEST_WRITE_WITH_TIMER:
         ui32Address = hexStringToInt(params[0]);
-        if (lSeek(ui32Address, gDeviceFd))
-        {
+        if (lSeek(ui32Address, gDeviceFd)) {
             emit reportlSeekError();
             return;
         }
@@ -147,19 +131,16 @@ void CmdHandlerZfpgaTest::StartCmd(SimpleCmdData *pCmd, QVariantList params)
         strData = fileData.replace(QLatin1String("\n"), QString());
 
         ui32Len = params[1].toInt();
-        for(quint32 ui32Byte=0; ui32Byte<ui32Len; ui32Byte++)
-        {
+        for(quint32 ui32Byte=0; ui32Byte<ui32Len; ui32Byte++) {
             writeData.append(strData.mid(ui32Byte*2, 2).toInt(Q_NULLPTR, 16));
         }
 
         quint32 iterations = params[3].toInt();
         std::vector<long> time_us;
         long avgTime_us = 0;
-        for(quint32 i=0; i<iterations; i++)
-        {
+        for(quint32 i=0; i<iterations; i++) {
             writeTime.start();
-            if(write(gDeviceFd, writeData.data(), ui32Len) < 0)
-            {
+            if(write(gDeviceFd, writeData.data(), ui32Len) < 0) {
                 emit OperationFinish(true, QLatin1String("write did not succeed"));
                 return;
             }
@@ -169,8 +150,7 @@ void CmdHandlerZfpgaTest::StartCmd(SimpleCmdData *pCmd, QVariantList params)
             strResult += strByteData;
         }
         auto minmaxTime = std::minmax_element(time_us.begin(), time_us.end());
-        for(const auto &time : time_us)
-        {
+        for(const auto &time : time_us) {
             avgTime_us += time;
         }
         avgTime_us /= time_us.size();
